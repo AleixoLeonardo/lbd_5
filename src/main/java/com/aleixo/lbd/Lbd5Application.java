646a3eb597111fa1ec4a51c24d8fd482ec040d26
LeonardoAleixo <leonardo.silva214@fatec.sp.gov.br>
2021-02-17 11:18:03 -0300

    configuração inicial

---
+++ src/main/java/com/aleixo/lbd/Lbd5Application.java
@@ -0,0 +1,13 @@
+package com.aleixo.lbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lbd5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lbd5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleixo/lbd/Lbd5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleixo.lbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lbd5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
